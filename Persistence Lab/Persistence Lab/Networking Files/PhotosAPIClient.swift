b98c9aadf3c2f94b899b401aa9d28e49119cf76e
Jason Ruan <jasonruan@pursuit.org>
2019-09-30 17:43:15 -0400

    Defines PhotosAPIClient

--- Persistence Lab/Persistence Lab/Networking Files/PhotosAPIClient.swift
+++ Persistence Lab/Persistence Lab/Networking Files/PhotosAPIClient.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+class PhotosAPIClient {
+    private init() {}
+    static let manager = PhotosAPIClient()
+    private let endpointString = "https://pixabay.com/api/?key=\(Secrets().apiKey)&q="
+    
+    func getPhotos(searchString: String?, completionHandler: @escaping (Result<[Photo], AppError>) -> () ) {
+        var urlString = endpointString
+        if let searchString = searchString {
+            urlString.append(searchString)
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+        
+        NetworkHelper.manager.performDataTask(withUrl: url, andHTTPBody: nil, andMethod: .get) { (result) in
+            switch result {
+            case .failure(_):
+                completionHandler(.failure(.noDataReceived))
+            case .success(let data):
+                do {
+                    let arrPhotos = try JSONDecoder().decode([Photo].self, from: data)
+                    completionHandler(.success(arrPhotos))
+                } catch {
+                    completionHandler(.failure(.couldNotParseJSON(rawError: error)))
+                }
+            }
+        }
+        
+    }
+    
+}
