1637f36dead7a5e19b5ad021bc12c827433458c3
Jason Ruan <jasonruan@pursuit.org>
2019-09-30 17:30:17 -0400

    Adds Photo and PhotosAPIClient files and defines Photo struct model

---
+++ Persistence Lab/Persistence Lab/Models/Photo.swift
@@ -0,0 +1,22 @@
+//
+//  PhotoHit.swift
+//  Persistence Lab
+//
+//  Created by Jason Ruan on 9/30/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoWrapper: Codable {
+    let hits: [Photo]
+    
+    struct Photo: Codable {
+        let user: String
+        let likes: Int
+        let favorites: Int
+        let tags: String
+        let previewURL: URL
+        let webformatURL: URL
+    }
+}
---
+++ Persistence Lab/Persistence Lab/Networking Files/PhotosAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  PhotosAPIClient.swift
+//  Persistence Lab
+//
+//  Created by Jason Ruan on 9/30/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import Foundation
